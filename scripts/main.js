86d1d26df532c1b0fc76d7614bbf585cd28b8ef8
kosorio2 <oso16001@byui.edu>
2021-01-23 12:08:13 -0700

    week 3

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,11 @@ const links = [
     },
     {
         label: "Week 2 notes",
-        url: "/week2/index.html"
+        url: "../week2/index.html"
+    },
+    {
+        label: "Week 3 notes",
+        url: "../week3/index.html"
     }
 ];
 
