3104e86b10d1da4db60d8cf4aa2d8856f34d9f72
kosorio2 <oso16001@byui.edu>
2021-02-03 19:13:26 -0700

    Notes on errors()

--- week5/scripts/week5.js
+++ week5/scripts/week5.js
@@ -20,3 +20,60 @@ function amIOldEnough(age){
 }
 
 amIOldEnough(21)
+
+//using the throw error 
+function squareRoot(number) {
+    'use strict';
+    if (number < 0) {
+        throw new RangeError('You cant find the square root of negative numbers')
+    }
+    return Math.sqrt(number);
+};
+
+console.log(squareRoot(121))
+console.log(squareRoot(-1))
+
+//using try, catch and finally 
+function imaginarySquareRoot(number) {
+    'use strict';
+    try {
+        return String(squareRoot(number));
+    } catch(error) {
+        return squareRoot(-number)+'i';
+    }
+}
+
+// console.log(imagineSquareRoot(-49)) 
+
+//Testing Jest 
+
+function squareRoot(number) {
+    'use strict';
+    if (number < 0) {
+        throw new RangeError("You can't find the square root of negative numbers")
+    }
+    return Math.sqrt(number);
+};
+
+test('square root of 4 is 2', () => {
+    expect(squareRoot(4)).toBe(2);
+});
+
+function factorsOf(n) {
+    const factors = [];
+    for (let i=1 , max = Math.sqrt(n); i <= max ; i++) {
+        if (n%i === 0) {
+            factors.push(i, n/i);
+        }
+    }
+    return factors.sort((a, b) => a - b);
+}
+
+function isPrime(n) {
+    return factorsOf(n).length === 2;
+}
+
+test('2 is prime', () => {
+    expect(isPrime(2)).toBe(true);
+});
+
